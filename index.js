77c00493a9a1050600da1dec5669d2268d38bd71
OSAMA-1329 <usamausmah@gmail.com>
2024-05-26 10:01:03 +0530

    coommit server read file

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 // console.log("Started server")
 
-
 // const express = require("express")
 // const app = express();
 // const PORT =3000;
-
-
 
 // app.get("/", (req,res)=>{
 //     console.log("success");
@@ -15,12 +12,44 @@
 // app.listen(PORT, ( )=> {
 //     console.log("Listening server");
 // })
+userData = [
+  {
+    name: "mro",
+    class: "fsd",
+    address: {
+      no: "3",
+      street: "vivekanandar street dubai main road",
+      city: "dubai",
+    },
+  },
+  {
+    name: "VIJAY",
+    class: "fsd",
+    address: {
+      no: "3",
+      street: "vivekanandar street dubai main road",
+      city: "dubai",
+    },
+  },
+];
+const http = require("http");
+const fs = require("fs");
 
-const http =require('http')
-PORT=8000;
-let server=new http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/html'})
-    res.end("hi")
-})
+PORT = 3000;
+let server = new http.createServer((req, res) => {
+  try {
+    let data = fs.readFile('node.txt', 'utf-8',(err, data) => {
+      if (err) {
+        throw err
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/html" });
+    res.end("tryagain later");
+  }
+});
 
-server.listen(PORT, ()=>console.log(`"Serrver listening ${PORT}"`))
+server.listen(PORT, () => console.log(`"Serrver listening ${PORT}"`));
